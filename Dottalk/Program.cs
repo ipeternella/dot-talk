19c5741ead6338c9b6d98ff4cdbf02a4b0aee526
IgooorGP <igor.feq@gmail.com>
2020-09-03 19:42:02 -0300

    Removed unused controllers.

--- Dottalk/Program.cs
+++ Dottalk/Program.cs
@@ -1,12 +1,8 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Serilog;
 
 namespace Dottalk
