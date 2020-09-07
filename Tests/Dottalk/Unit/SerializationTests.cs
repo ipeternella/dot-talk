e737996ef9380d1394d315c06dfaa4ba02bd7e02
IgooorGP <igor.feq@gmail.com>
2020-09-07 11:07:30 -0300

    Fixed serialization tests.

--- Tests/Dottalk/Unit/SerializationTests.cs
+++ Tests/Dottalk/Unit/SerializationTests.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections.Generic;
-using Dottalk.App.DTOs;
+using Dottalk.App.Domain.Models;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json.Serialization;
@@ -26,29 +26,31 @@ namespace Tests.Dottalk.Unit
         public void TestShouldSerializerConnectionObject()
         {
             // arrange
-            var serverInstance1 = Guid.NewGuid();
-            var serverInstance2 = Guid.NewGuid();
+            var chatRoomId = Guid.NewGuid();
+            var serverInstanceId1 = Guid.NewGuid();
+            var serverInstanceId2 = Guid.NewGuid();
             var connectedUserId1 = Guid.NewGuid();
             var connectedUserId2 = Guid.NewGuid();
             var connectedUserId3 = Guid.NewGuid();
             var connectedUserId4 = Guid.NewGuid();
 
-            var serverInstanceUsers1 = new ServerInstanceUsersDTO()
+            var serverInstance1 = new ServerInstance()
             {
-                ServerInstanceId = serverInstance1,
+                ServerInstanceId = serverInstanceId1,
                 ConnectedUsers = new List<Guid>() { connectedUserId1, connectedUserId2 }
             };
 
-            var serverInstanceUsers2 = new ServerInstanceUsersDTO()
+            var serverInstance2 = new ServerInstance()
             {
-                ServerInstanceId = serverInstance2,
+                ServerInstanceId = serverInstanceId2,
                 ConnectedUsers = new List<Guid>() { connectedUserId3, connectedUserId4 }
             };
 
-            var chatRoomConnections = new ChatRoomConnectionsDTO()
+            var chatRoomConnections = new ChatRoomConnections()
             {
+                ChatRoomId = chatRoomId,
                 TotalActiveConnections = 4,
-                ServerInstanceUsers = new List<ServerInstanceUsersDTO>() { serverInstanceUsers1, serverInstanceUsers2 }
+                ServerInstances = new List<ServerInstance>() { serverInstance1, serverInstance2 }
             };
 
             // act -- serializes a complex object using the app settings
@@ -56,15 +58,16 @@ namespace Tests.Dottalk.Unit
 
             // assert - guarantees camelCase and not PascalCase on the jsons
             var jsonChatRoomConnection = JObject.Parse(serializedChatRoomConnectionsStr);
-            var serverInstances = (JArray)jsonChatRoomConnection["serverInstanceUsers"];
+            var serverInstances = (JArray)jsonChatRoomConnection["serverInstances"];
 
+            Assert.Equal(chatRoomId.ToString(), jsonChatRoomConnection["chatRoomId"]);
             Assert.Equal(4, jsonChatRoomConnection["totalActiveConnections"].Value<int>());
             Assert.Equal(2, serverInstances.Count);
 
-            Assert.Equal(serverInstance1.ToString(),
-                jsonChatRoomConnection["serverInstanceUsers"][0]["serverInstanceId"].Value<string>());
-            Assert.Equal(serverInstance2.ToString(),
-                jsonChatRoomConnection["serverInstanceUsers"][1]["serverInstanceId"].Value<string>());
+            Assert.Equal(serverInstanceId1.ToString(),
+                jsonChatRoomConnection["serverInstances"][0]["serverInstanceId"].Value<string>());
+            Assert.Equal(serverInstanceId2.ToString(),
+                jsonChatRoomConnection["serverInstances"][1]["serverInstanceId"].Value<string>());
         }
     }
 }
