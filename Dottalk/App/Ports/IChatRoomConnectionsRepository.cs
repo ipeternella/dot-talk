bc498eeb0bc5476ed401b4f2e3feeb47e7ad67ea
IgooorGP <igor.feq@gmail.com>
2020-09-07 11:08:28 -0300

    Added chat room conns repository (WIP).

--- Dottalk/App/Ports/IChatRoomConnectionsRepository.cs
+++ Dottalk/App/Ports/IChatRoomConnectionsRepository.cs
@@ -1,16 +1,17 @@
 using System;
-using Dottalk.App.DTOs;
+using Dottalk.App.Domain.Models;
 
 namespace Dottalk.App.Ports
 {
+    //
     // Summary:
     //   Interface used to interact with a memory cache/Redis in order to retrieve all hosting nodes
     //   that are hosting a chat room. Works closely with the ChatRoomConnections DTO.
     public interface IChatRoomConnectionsRepository
     {
-        public void AddConnection(Guid chatRoomId, Guid userId, Guid nodeId);
-        public void RemoveConnection(Guid chatRoomId, Guid userId, Guid nodeId);
+        public void AddChatRoomConnection(Guid chatRoomId, Guid userId, Guid ServerInstanceId);
+        public void RemoveChatRoomConnection(Guid chatRoomId, Guid userId, Guid ServerInstanceId);
         public int TotalActiveConnections(Guid chatRoomId);
-        public ChatRoomConnectionsDTO GetChatRoomConnections(Guid chatRoomId);
+        public ChatRoomConnections GetChatRoomConnections(Guid chatRoomId);
     }
 }
--- Dottalk/Infra/Persistence/ChatRoomConnectionsRepository.cs
+++ Dottalk/Infra/Persistence/ChatRoomConnectionsRepository.cs
@@ -1,9 +1,50 @@
+using System;
+using Dottalk.App.Domain.Models;
+using Dottalk.App.Ports;
+using Microsoft.Extensions.Configuration;
+using StackExchange.Redis;
+
 namespace Dottalk.Infra.Persistence
 {
+    //
     // Summary:
-    //   Repository for managing chat room connections that are sustained through several nodes.
-    public class ChatRoomConnectionsRepository
+    //   Repository for managing chat room connections that are sustained through several application instances.
+    public class ChatRoomConnectionsRepository : IChatRoomConnectionsRepository
     {
+        private readonly ConnectionMultiplexer _redisConnection;
+        private readonly IDatabase _redisDb;
 
+        public ChatRoomConnectionsRepository(IConfiguration configuration)
+        {
+            var redisConnectionString = configuration.GetValue<string>("Redis:ConnectionString");
+            var redisDatabaseNumber = configuration.GetValue<int>("Redis:ConnectionRepositoryDatabaseNumber");
+
+            if (redisConnectionString == null || redisDatabaseNumber == 0)
+                throw new Exception("Redis connection is misconfigured!");
+
+            _redisConnection = ConnectionMultiplexer.Connect(redisConnectionString);
+            _redisDb = _redisConnection.GetDatabase(redisDatabaseNumber);
+        }
+
+        public void AddChatRoomConnection(Guid chatRoomId, Guid userId, Guid ServerInstanceId)
+        {
+            // now we know how to serialize DTOs...
+            throw new NotImplementedException();
+        }
+
+        public void RemoveChatRoomConnection(Guid chatRoomId, Guid userId, Guid ServerInstanceId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int TotalActiveConnections(Guid chatRoomId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ChatRoomConnections GetChatRoomConnections(Guid chatRoomId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
