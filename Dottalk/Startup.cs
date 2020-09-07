c60bbe23c3410a51a824960263ee46fd3e08d9d4
IgooorGP <igor.feq@gmail.com>
2020-09-06 21:35:52 -0300

    Configured Newtonsoft Json serializer

--- Dottalk/Startup.cs
+++ Dottalk/Startup.cs
@@ -1,10 +1,14 @@
 using System;
+using System.Collections.Generic;
+using Dottalk.App.DTOs;
 using Dottalk.Controllers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using Serilog;
 
 namespace Dottalk
@@ -20,9 +24,21 @@ namespace Dottalk
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddHttpContextAccessor();
-            services.AddControllers();
+            services.AddHttpContextAccessor()
+                .AddControllers();
+
             services.AddSignalR();
+
+            // global json serialization settings
+            JsonConvert.DefaultSettings = () => new JsonSerializerSettings
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                ContractResolver = new DefaultContractResolver
+                {
+                    NamingStrategy = new CamelCaseNamingStrategy()
+                }
+            };
+
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
