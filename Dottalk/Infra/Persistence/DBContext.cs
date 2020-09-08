6a51b077ec84b92f5dcbaf8b437f2099ce7d9c03
IgooorGP <igor.feq@gmail.com>
2020-09-08 20:34:23 -0300

    Added SQL DB context for the app.

---
+++ Dottalk/Infra/Persistence/DBContext.cs
@@ -0,0 +1,54 @@
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Dottalk.App.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dottalk.Infra.Persistence
+{
+    //
+    //  Summary:
+    //    The main SQL database context of the application.
+    public class DBContext : DbContext
+    {
+        public DbSet<User> Users { get; set; } = null!;
+        public DbSet<ChatRoom> ChatRooms { get; set; } = null!;
+        public DbSet<ChatMessage> ChatMessages { get; set; } = null!;
+
+        public DBContext(DbContextOptions<DBContext> options) : base(options)
+        {
+
+        }
+        //
+        //  Summary:
+        //    Thin wrapper on top of save async in order to automatically add createdAt and updatedAt fields.
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            AutomaticallyAddCreatedAndUpdatedAt();
+            return base.SaveChangesAsync(cancellationToken);
+        }
+        //
+        //  Summary:
+        //    Automatically adds the createdAt and updatedAt fields for models that are persisted 
+        //    on the database.
+        private void AutomaticallyAddCreatedAndUpdatedAt()
+        {
+            var entitiesOnDbContext = ChangeTracker.Entries<BaseEntity>();
+
+            if (entitiesOnDbContext == null) return; // nothing was changed on DB context
+
+            // createdAt addition
+            foreach (var item in entitiesOnDbContext.Where(t => t.State == EntityState.Added))
+            {
+                item.Entity.CreatedAt = System.DateTime.Now;
+                item.Entity.UpdatedAt = System.DateTime.Now;
+            }
+
+            // updatedAt addition
+            foreach (var item in entitiesOnDbContext.Where(t => t.State == EntityState.Modified))
+            {
+                item.Entity.UpdatedAt = System.DateTime.Now;
+            }
+        }
+    }
+}
