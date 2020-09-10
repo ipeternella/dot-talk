8b1a6666f506c0b8ea3eb142a0367f45e4c6349e
IgooorGP <igor.feq@gmail.com>
2020-09-10 18:58:42 -0300

    Setup migrations on start up.

--- Dottalk/Startup.cs
+++ Dottalk/Startup.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Linq;
+using Dottalk.App.Domain.Models;
 using Dottalk.Controllers;
 using Dottalk.Infra.Persistence;
 using Microsoft.AspNetCore.Builder;
@@ -6,6 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using Serilog;
@@ -39,10 +43,9 @@ namespace Dottalk
                     NamingStrategy = new CamelCaseNamingStrategy()
                 }
             };
-
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
         {
             if (!env.IsProduction())
             {
@@ -60,6 +63,49 @@ namespace Dottalk
                 endpoints.MapControllers();
                 endpoints.MapHub<ChatHubController>("/chathub");
             });
+
+            Migrate(app, logger, executeSeedDb: env.IsDevelopment());
+        }
+
+        public static void Migrate(IApplicationBuilder app, ILogger<Startup> logger, bool executeSeedDb = false)
+        {
+            using var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope();
+            using var context = serviceScope.ServiceProvider.GetService<DBContext>();
+
+            // always execute possible missing migrations
+            if (context.Database.GetPendingMigrations().ToList().Any())
+            {
+                logger.LogInformation("Applying migrations...");
+                context.Database.Migrate();
+            }
+
+            // seeding DB only when asked
+            if (!executeSeedDb) return;
+
+            logger.LogInformation("Seeding the database...");
+            SeedDb(context, logger);
+        }
+
+        private static void SeedDb(DBContext context, ILogger<Startup> logger)
+        {
+            if (context.ChatRooms.Any())
+            {
+                logger.LogInformation("Database has already been seeded. Skipping it...");
+                return;
+            }
+
+            logger.LogInformation("Saving entities...");
+            var chatRooms = new List<ChatRoom>
+            {
+                new ChatRoom {Name = "Chat Room 1"},
+                new ChatRoom {Name = "Chat Room 2"},
+                new ChatRoom {Name = "Chat Room 3"}
+            };
+
+            context.AddRange(chatRooms);
+            context.SaveChanges();
+
+            logger.LogInformation("Database has been seeded successfully.");
         }
     }
 }
