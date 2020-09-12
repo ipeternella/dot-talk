331d12686a85fdcfc2bc9f97d9cdcc9547bd1963
IgooorGP <igor.feq@gmail.com>
2020-09-12 12:14:43 -0300

    Added more fields to ChatRoomResponseDTO.

--- Dottalk/App/DTOs/ChatRoomDTOs.cs
+++ Dottalk/App/DTOs/ChatRoomDTOs.cs
@@ -1,3 +1,4 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 using Newtonsoft.Json;
 
@@ -11,7 +12,10 @@ namespace Dottalk.App.DTOs
 
     public class ChatRoomCreationResponseDTO
     {
+        public Guid Id { get; set; }
         public string Name { get; set; } = default!;
         public int ActiveConnectionsLimit { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
     }
 }
